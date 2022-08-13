164b84837063761d8e8ff4a53f88bfa90d497df7
Rafael Neuwirth Swierczynski <channelrns@gmail.com>
2022-08-13 12:59:49 -0300

    Initial Commit

---
+++ iOS LengthConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS LengthConverter
+//
+//  Created by Rafael Neuwirth Swierczynski on 13/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS LengthConverter/iOS_LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_LengthConverterApp.swift
+//  iOS LengthConverter
+//
+//  Created by Rafael Neuwirth Swierczynski on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
